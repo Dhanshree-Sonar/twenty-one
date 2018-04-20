381c78108644d2cd849f2fce8e1502f29a1631ed
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-04-19 23:13:35 -0400

    Feat: Add steps to test scenario 2
    Added steps to test scenario, Get hand total.

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -12,3 +12,16 @@ def step_impl(context):
 @then('the dealer gives itself two cards')
 def step_impl(context):
     assert (len(context.dealer.hand) == 2)
+
+@given('a {hand}')
+def step_impl(context, hand):
+    context.dealer = Dealer()
+    context.dealer.hand = hand.split(',')
+
+@when('the dealer sums the cards')
+def step_impl(context):
+    context.dealer_total = context.dealer.get_hand_total()
+
+@then('the {total:d} is correct')
+def step_impl(context, total):
+    assert (context.dealer_total == total)
