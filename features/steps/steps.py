45ec89bf1be9b30534f021be5402d0841f252c04
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-04-26 22:38:13 -0400

    Feat: Add a when step

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -15,17 +15,23 @@ def step_impl(context, hand):
     context.dealer = Dealer()
     context.dealer.hand = hand.split(',')
 
+
 @when('the round starts')
 def step_impl(context):
     context.dealer.new_round()
 
-@then('the dealer gives itself two cards')
+@when('the dealer determines a play')
 def step_impl(context):
-    assert (len(context.dealer.hand) == 2)
+    context.dealer_play = context.dealer.determine_play(context.total)
 
 @when('the dealer sums the cards')
 def step_impl(context):
     context.dealer_total = context.dealer.get_hand_total()
+
+
+@then('the dealer gives itself two cards')
+def step_impl(context):
+    assert (len(context.dealer.hand) == 2)
 
 @then('the {total:d} is correct')
 def step_impl(context, total):
